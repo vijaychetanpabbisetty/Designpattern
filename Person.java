c9b81b3597ba7f2e5879e204039171440ec4ab0a
smanda38 <smanda38@asu.edu>
2022-10-18 12:10:36 +0530

    first commit

---
+++ Person.java
@@ -0,0 +1,31 @@
+public class Person {
+
+	private ProductMenu theProductMenu;
+
+	private ProductMenu productMenu;
+
+	public void showMenu() {
+
+	}
+
+	public void showAddButton() {
+
+	}
+
+	public void showViewButton() {
+
+	}
+
+	public void showRadioButton() {
+
+	}
+
+	public void showLabels() {
+
+	}
+
+	public void CreateProductMenu() {
+
+	}
+
+}
---
+++ ProductMenu.java
@@ -0,0 +1,17 @@
+public interface ProductMenu {
+
+	private Person person;
+
+	public abstract void showMenu();
+
+	public abstract void showAddButton();
+
+	public abstract void showViewButton();
+
+	public abstract void showRadioButton();
+
+	public abstract void showLabels();
+
+	public abstract void showComboxes();
+
+}
